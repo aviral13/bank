d291ae4e7809f8007e6e716f077bcc088eb18123
Aviral Garg <67217866+aviral13@users.noreply.github.com>
2021-09-20 11:12:29 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+$servername = "localhost";
+$username="root";
+$password="";
+$database="banking";
+
+	$conn = mysqli_connect($servername,$username,$password,$database);
+
+	if(!$conn){
+		die("Could not connect to the database due to the following error --> ".mysqli_connect_error());
+	}
+
+?>
---
+++ index.php
@@ -0,0 +1,97 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/navbar.css">
+    
+    <title>Indraprastha Bank</title>
+  </head>
+
+  <body>
+  <?php
+  include 'navbar.php';
+  ?>
+  
+      <!-- Introduction section -->
+      <section class="introduction_section">
+  <div class="about">
+    <div class="about-text">
+    <h3>Welcome to</h3>
+                  <h1>INDRAPRASTHA BANK</h1>
+                  
+                  <a href="createuser.php"><button>Join Us!</button></a>
+                  
+    </div>
+    <div class="about-image">
+    <img class="image-about" src="http://www.india.com/wp-content/uploads/2014/08/666.jpg">
+    </div>
+  </div>
+</section>
+<!-- About us section -->
+<div class="about-us">
+              <div class="about-us-inner">
+                <h1 >About Us</h1>
+                <h3>Our Values</h3>
+                <p>
+                We are committed to building and sustaining trusted relationships.
+</br>
+
+• Integrity
+</br>
+
+We do what is right for our employees, our customers, our shareholders and our communities – 100% of the time.
+</br>
+• Hard Work
+</br>
+We strive to out-perform our competition with diligence, efficiency and accuracy.
+</br>
+• Teamwork
+</br>
+We work together to accomplish our company’s vision, mission and our customer’s financial goals.
+</br>
+• Caring
+</br>
+We display kindness and respect to each other and our customers.
+                </p>
+
+              </div>
+          </div>
+
+      <!-- Activity section -->
+      <div class="our-services">
+        <div class="heading">
+          <h1>Our Services</h1>
+        </div>
+        <div class="services">
+          <div class="service">
+          <img src="https://png.pngtree.com/png-vector/20200614/ourlarge/pngtree-businessman-user-avatar-character-vector-illustration-png-image_2242909.jpg" class="img-fluid">
+                    <br>
+                    <a href="createuser.php"><button>Create a User</button></a>
+          </div>
+          <div class="service">
+          <img src="https://png.pngtree.com/png-vector/20190704/ourmid/pngtree-transaction-icon-in-trendy-style-isolated-background-png-image_1540305.jpg" class="img-fluid">
+                    <br>
+                    <a href="transfermoney.php"><button>Make a Transaction</button></a>
+          </div>
+          <div class="service">
+          <img src="https://thumbs.dreamstime.com/b/transaction-history-icon-transaction-history-icon-simple-vector-illustration-215661773.jpg" class="img-fluid">
+                    <br>
+                    <a href="transactionhistory.php"><button>Transaction History</button></a>
+          </div>
+        </div>
+
+      </div>
+  <!-- footer -->
+      <footer class="text-center mt-5 py-2">
+        <p>&copy 2021 Made by <b>ADITI RAI</b> <br> INDRAPRASTHA BANK</p>
+      </footer>
+      <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+  </body>
+</html>
